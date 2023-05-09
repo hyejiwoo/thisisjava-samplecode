d39b7688f191102386e74af134760acf9271ec99
hyejiwoo <hyejiii.woo@gmail.com>
2023-05-10 00:35:57 +0900

    docs

--- src/ch05/sec06/ArrayLengthExample.java
+++ src/ch05/sec06/ArrayLengthExample.java
@@ -5,7 +5,7 @@ public class ArrayLengthExample {
         //배열 변수 선언과 배열 대입
         int[] scores = {84,90,96};
 
-        //배열 항목의 총합 구하기 d
+        //배열 항목의 총합 구하기
         int sum = 0;
         for(int i=0; i<scores.length; i++){
             sum += scores[i];
